2c7084f83f772d5fc26c2973b4e0ad58ccb76c38
870pjh <kanaldo@KaNaldos-MacBook-Air.local>
2022-08-11 16:15:40 +0900

    미리보기와 요약문 추가하기

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 import os
 class Post(models.Model):
     title = models.CharField(max_length=30)
+    hook_text = models.CharField(max_length=100, blank=True)
     content = models.TextField()
 
     created_at = models.DateTimeField(auto_now_add=True)
@@ -9,6 +10,8 @@ class Post(models.Model):
 
     head_image = models.ImageField(upload_to='blog/images/%Y/%d/', blank=True)
     file_upload = models.FileField(upload_to='blog/files/%Y/%d/', blank=True)
+
+
 
 # Create your models here.
 
