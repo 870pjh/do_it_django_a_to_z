c81c20118f63ede93e198f796fc16e699490e17f
870pjh <kanaldo@KaNaldos-MacBook-Air.local>
2022-08-16 10:18:37 +0900

    작성자 필드 추가

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 import os
 class Post(models.Model):
     title = models.CharField(max_length=30)
@@ -11,12 +12,12 @@ class Post(models.Model):
     head_image = models.ImageField(upload_to='blog/images/%Y/%d/', blank=True)
     file_upload = models.FileField(upload_to='blog/files/%Y/%d/', blank=True)
 
-
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
 # Create your models here.
 
     def __str__(self):
-        return f'[{self.pk}]{self.title}'
+        return f'[{self.pk}]{self.title} :: {self.author}'
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
 
